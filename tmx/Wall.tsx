33b9ae79075d3df5bd5edb78ed172a3e25c34dbb
QtDesiner <118472306+QtDesiner@users.noreply.github.com>
2023-01-16 15:04:15 +0300

    Add files via upload
    давай давай

---
+++ tmx/Wall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Wall" tilewidth="50" tileheight="50" tilecount="1" columns="1">
+ <image source="wall.png" width="50" height="50"/>
+</tileset>
---
+++ tmx/buch-outdoor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="buch-outdoor" tilewidth="15" tileheight="15" spacing="1" margin="1" tilecount="288" columns="24">
+ <image source="../../../../../Program Files/Tiled/examples/buch-outdoor.png" width="384" height="192"/>
+</tileset>
